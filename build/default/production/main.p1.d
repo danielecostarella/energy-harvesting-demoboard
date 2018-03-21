a5d4f077e7c412e8debda5042d2a81123e3822e6
Daniele Costarella <daniele.costarella@gmail.com>
2018-03-21 22:49:23 +0100

    Remove built files

--- build/default/production/main.p1.d
+++
@@ -1,5 +0,0 @@
- build/default/production/main.d  \
- build/default/production/main.p1:  \
- main.c  \
-rfm70.h  \
-main.h 
--- build/default/production/rfm70.p1.d
+++
@@ -1,5 +0,0 @@
- build/default/production/rfm70.d  \
- build/default/production/rfm70.p1:  \
- rfm70.c  \
-rfm70.h  \
-main.h 
